21efde3b874d4d63fc458bbf6c7ca4ef2ee7e671
Hitesh Prajapati <hittu9173@gmail.com>
2025-09-10 18:22:43 +0530

    Update Database-Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,19 +5,28 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+	/**
+	 * Seed the application's database.
+	 */
+	public function run(): void
+	{
+		// User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+		User::updateOrCreate(
+			['email' => 'test@example.com'],
+			[
+				'name' => 'Hitesh Prajapati',
+				'password' => Hash::make('Reset@123'),
+				'email_verified_at' => now(),
+			]
+		);
+
+		$this->call([
+			\Database\Seeders\ProductSeeder::class,
+		]);
+	}
 }
